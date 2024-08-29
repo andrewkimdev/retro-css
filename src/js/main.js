4c4f65248aaf4e44b5fe981996d9c7181c9bc30c
Andrew B Kim <andrewbkim.dev@gmail.com>
2024-08-29 14:28:14 +0900

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// Import our custom CSS
+import '../scss/bootstrap.scss';
+// import '../assets/fonts/fontawesome-free/scss/font-awesome.scss';
+import '../assets/fonts/fontawesome-free/scss/font-awesome.scss';
+
+import '../scss/styles.scss';
+
+// Import all of Bootstrap's JavaScript
+import * as bootstrap from 'bootstrap';
